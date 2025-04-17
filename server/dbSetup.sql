1762ac0b62429c115f376d54c7c16d68d859d1a5
Roman <romanjolsen@gmail.com>
2025-04-17 14:30:32 -0600

    sql tables created

--- server/dbSetup.sql
+++ server/dbSetup.sql
@@ -7,6 +7,8 @@ CREATE TABLE IF NOT EXISTS accounts(
   picture VARCHAR(255) COMMENT 'User Picture'
 ) default charset utf8mb4 COMMENT '';
 
+select * from accounts;
+
 CREATE TABLE recipes(
   id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
@@ -15,9 +17,34 @@ CREATE TABLE recipes(
   instructions varchar(5000), 
   img varchar(1000) NOT NULL,
   category ENUM('breakfast', 'lunch', 'dinner', 'snack', 'dessert') NOT NULL,
-  creator_id varchar(255) NOT NULL
+  FOREIGN KEY (creator_id) REFERENCES accounts(id) ON DELETE CASCADE
 );
 
 SELECT * FROM recipes
 
 DROP TABLE recipes;
+
+CREATE TABLE ingredients(
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
+  name varchar(255) NOT NULL,
+  quantity varchar(255) NOT NULL,
+  -- recipe_id INT FOREIGN KEY AUTO_INCREMENT NOT NULL -- later on change this to match recipe
+  recipe_id int not null,
+  FOREIGN KEY (recipe_id) REFERENCES recipes(id) ON DELETE CASCADE
+);
+
+select * from ingredients;
+
+CREATE TABLE favorites(
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
+  recipe_id int not null,
+  FOREIGN KEY (recipe_id) REFERENCES recipes(id) ON DELETE CASCADE,
+  account_id varchar(255) not null,
+  FOREIGN KEY (account_id) REFERENCES accounts(id) ON DELETE CASCADE
+);
+
+select * from favorites;
