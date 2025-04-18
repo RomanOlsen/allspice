63db8a4f34a81baee3aabdd4be977b208480a450
Roman <romanjolsen@gmail.com>
2025-04-18 15:55:58 -0600

    getting back correct information but postman not complete

--- server/Repositories/FavoritesRepository.cs
+++ server/Repositories/FavoritesRepository.cs
@@ -15,14 +15,16 @@ public class FavoritesRepository
     INSERT INTO favorites (recipe_id, account_id) 
     VALUES (@RecipeId, @AccountId);
     
-    SELECT favorites.*, recipes.* FROM favorites
+    SELECT favorites.*, recipes.*, accounts.* FROM favorites
      INNER JOIN recipes ON recipes.id = favorites.recipe_id
+     INNER JOIN accounts ON accounts.id = favorites.account_id
      WHERE favorites.id = LAST_INSERT_ID();
     
     ";
 
-    Favorite favorite = _db.Query(sql, (FavoriteRecipe favorite, Account account) =>
+    Favorite favorite = _db.Query(sql, (FavoriteRecipe favorite, Recipe recipe, Account account) =>
     {
+      favorite.Recipe = recipe;
       favorite.Creator = account;
       return favorite;
     }, favoriteData).SingleOrDefault();
