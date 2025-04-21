c95176e27c5930c6a99e88f62ce56c148cd74b4d
Roman <romanjolsen@gmail.com>
2025-04-21 14:54:10 -0600

    hero section

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -5,9 +5,9 @@ import Login from './Login.vue';
 
 const theme = ref(loadState('theme') || 'light')
 
-function toggleTheme() {
-  theme.value = theme.value == 'light' ? 'dark' : 'light'
-}
+// function toggleTheme() {
+//   theme.value = theme.value == 'light' ? 'dark' : 'light'
+// }
 
 watch(theme, () => {
   document.documentElement.setAttribute('data-bs-theme', theme.value)
@@ -17,11 +17,11 @@ watch(theme, () => {
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-md bg-codeworks border-bottom border-vue">
+  <nav class="navbar navbar-expand-md bg-codeworks">
     <div class="container gap-2">
       <RouterLink :to="{ name: 'Home' }" class="d-flex align-items-center text-light">
-        <img class="navbar-brand" alt="logo" src="/img/cw-logo.png" height="45" />
-        <b class="fs-5">Vue Starter</b>
+        <!-- <img class="navbar-brand" alt="logo" src="/img/cw-logo.png" height="45" /> -->
+        <b class="fs-5">All Spice</b>
       </RouterLink>
       <!-- collapse button -->
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-links"
@@ -29,7 +29,7 @@ watch(theme, () => {
         <span class="mdi mdi-menu text-light"></span>
       </button>
       <!-- collapsing menu -->
-      <div class="collapse navbar-collapse " id="navbar-links">
+      <div class="collapse navbar-collapse" id="navbar-links">
         <ul class="navbar-nav">
           <li>
             <RouterLink :to="{ name: 'About' }" class="btn text-green selectable">
@@ -39,16 +39,32 @@ watch(theme, () => {
         </ul>
         <!-- LOGIN COMPONENT HERE -->
         <div class="ms-auto">
-          <button class="btn text-light" @click="toggleTheme"
+          <!-- <button class="btn text-light" @click="toggleTheme"
             :title="`Enable ${theme == 'light' ? 'dark' : 'light'} theme.`">
             <i v-if="theme == 'dark'" class="mdi mdi-weather-sunny"></i>
             <i v-if="theme == 'light'" class="mdi mdi-weather-night"></i>
-          </button>
+          </button> -->
         </div>
         <Login />
       </div>
     </div>
   </nav>
+  <div class="container-fluid navbarPictureBg">
+    <Login />
+    <div class="row navbarPicture text-light sahitya-regular">
+      <div class="col-12 d-flex align-items-center flex-column justify-content-center">
+        <h1 class="">All-spice</h1>
+        <h2 class=""> Cherish your cooking and their family </h2>
+      </div>
+      
+    </div>
+  </div>
+
+
+
+
+
+
 </template>
 
 <style lang="scss" scoped>
@@ -64,5 +80,21 @@ a {
   border-bottom: 2px solid var(--bs-success);
   border-bottom-left-radius: 0;
   border-bottom-right-radius: 0;
+}
+
+.navbarPicture {
+  // object-fit: cover;
+  width: 100%;
+  margin: none;
+  padding: none;
+  height: 40dvh;
+  // object-fit: cover;
+  // transform: rotate(90deg);
+}
+.navbarPictureBg{
+  background-image: url("https://images.unsplash.com/photo-1612549225312-900aa64d56bb?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+  background-size: cover;
+  background-position: center;
+
 }
 </style>
