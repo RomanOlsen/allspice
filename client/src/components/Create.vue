ee15cab21589fa2ff249966a2d8476db0dbc693c
Roman <romanjolsen@gmail.com>
2025-04-21 16:16:16 -0600

    position fixed -- how we made the create button

---
+++ client/src/components/Create.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+</script>
+
+
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <button class="btn btn-success create-recipe">+</button>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+.create-recipe {
+  position: fixed;
+  bottom: 3%;
+  right: 2%;
+}
+</style>
